f86b78fceda7f83abe8d609c0eb542d9060a22ad
Andy Slater <aslater3@googlemail.com>
2026-08-25 14:06:37 +0100

    fix: point GitHub link to LibreEcho repository

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -6,6 +6,7 @@ CFG=./build/test-suite-config.json
 rm -f "$CFG" "$CFG.bak" "$CFG.tmp"
 cc -D_POSIX_C_SOURCE=200809L -std=c99 -Isrc tests/test_unit.c src/json.c src/config_store.c -o build/test-unit
 ./build/test-unit
+python3 tests/test_github_link_contract.py
 make build/test-auth-sessions
 ./build/test-auth-sessions
 sh tests/test_pr137_review_contract.sh
