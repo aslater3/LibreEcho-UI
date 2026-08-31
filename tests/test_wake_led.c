ffcb5ac85ae991de52cf68673348c47ba78012b9
Andy Slater <aslater3@googlemail.com>
2026-08-31 21:58:54 +0100

    test: align 0.13.9 UI contracts

--- tests/test_wake_led.c
+++ tests/test_wake_led.c
@@ -8,7 +8,7 @@ static int status_contains(const char *socket_path, const char *needle)
 {
     struct le_adapter *adapter =
         le_adapter_connect(socket_path, 500);
-    char status[1024];
+    char status[LE_ADAPTER_MSG_MAX];
     int result;
 
     if (!adapter)
